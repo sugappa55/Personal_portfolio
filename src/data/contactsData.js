8b4540e5a44e3b5c3e3a02fecad991291b34020e
Sugappa Kuruba <kurubasugappa@gmail.com>
2022-06-22 10:01:05 +0530

    data updated

---
+++ src/data/contactsData.js
@@ -0,0 +1,20 @@
+export const contactsData = {
+  email: "kurubasugappa@gmail.com",
+  phone: "+91 9652803979",
+  address: "Anantapur, Andhra Pradesh",
+
+  sheetAPI:
+    // "https://docs.google.com/spreadsheets/d/e/2PACX-1vRP1_DZcoT6Xvp4QkMDR4Fa1dZ99MerUjhjilqDHHjNJQgflR3F65poj-U-wS1R9AiM_zTzjpohGd6h/pubhtml",
+    // "
+   " https://getform.io/f/4f9b1045-e2f4-4b7a-9acc-3d149fcb9c1f"
+};
+
+// "Request is missing required authentication credential. Expected OAuth 2 access token, login cookie or other valid authentication credential. See https://developers.google.com/identity/sign-in/web/devconsole-project."
+
+
+// api key = AIzaSyDrBVNualoltTWOI5Mkxp4sAP3c85rnNTE
+
+// client Id = 686410709267-gr273ob93mn6v7qavfhv7oculrc4hscq.apps.googleusercontent.com
+// client secret = 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
+
+
---
+++ src/data/projectsData.js
@@ -0,0 +1,73 @@
+// import one from "../assets/svg/projects/one.svg";
+// import two from "../assets/svg/projects/two.svg";
+// import three from "../assets/svg/projects/three.svg";
+// import four from "../assets/svg/projects/four.svg";
+// import five from "../assets/svg/projects/five.svg";
+// import six from "../assets/svg/projects/six.svg";
+// import seven from "../assets/svg/projects/seven.svg";
+// import eight from "../assets/svg/projects/eight.svg";
+// import udemy from "../assets/png/udemy2.jpeg";
+
+export const projectsData = [
+  {
+    id: 1,
+    projectName: "Crypto Tracking App",
+    projectDesc: [
+      `A cryptocurrency  tracker is a website,that allows you to keep track of how the value of coins are changing.`,
+      [
+        "Google authentication with help of firebase",
+        "Responsive Web pages",
+        "Major bug fixes",
+      ],
+    ],
+    tags: ["React", "materialui", "firebase"],
+    code: "https://github.com/sugappa55/Crypto-Tracker",
+    demo: "https://crypto-tracker-vert.vercel.app/",
+    image:
+      "https://i.postimg.cc/Hsvmgr2S/Screenshot-155.png",
+  },
+  {
+    id: 2,
+    projectName: "Netflix frontend Clone",
+    projectDesc: [
+      "Mini clone of netflix.com with user authentication and add to watchlist functionalities",
+      ["Responsive webpages", "Fully functional cart", "Major Bug fixes"],
+    ],
+    tags: ["React", "tailwind", "firebase"],
+    code: "https://github.com/sugappa55/Netflix-Clone",
+    demo: "https://netflix-react-f90d2.web.app/",
+    image:"https://i.postimg.cc/SKNczt8Q/Screenshot-153.png"
+  },
+  {
+    id: 3,
+    projectName: "Home to go clone",
+    projectDesc: [
+      "Clone of Hometogo.com Website which provides rental rooms for Vacational Purposes.",
+      [
+        "Built details page",
+        "Bulit the backend api",
+        "Added functionality to cart",
+      ],
+    ],
+    tags: ["mongodb", "node js", "express"],
+    code: "https://github.com/deepakdk0808/home2go-FRONTENDapi",
+    demo: "https://home2go-frontend-api.vercel.app/",
+    image:"https://i.postimg.cc/rFNBVK96/hometogo.png"
+     // "https://raw.githubusercontent.com/Renukote/clone-rodanandfields.com/master/rodan-and-fields-thumbnail.png",
+  },
+];
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
